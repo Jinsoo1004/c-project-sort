4523a041bad2250ac9d81c217d640511d22d2678
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2022-11-21 12:36:30 +0900

    c언어

---
+++ c-suhanc/cmake-build-debug/CMakeFiles/c_suhanc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_suhanc.dir/main.c.obj"
+  "CMakeFiles/c_suhanc.dir/main.c.obj.d"
+  "c_suhanc.exe"
+  "c_suhanc.exe.manifest"
+  "c_suhanc.pdb"
+  "libc_suhanc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_suhanc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
